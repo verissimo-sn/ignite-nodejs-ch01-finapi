d4d2268664b40cd979ff74156e06bb5746dd1110
verissimo-sn <jose.neto@minu.co>
2022-01-03 11:56:44 -0300

    feat: create account

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuid();
+
+  const newAccount = {
+    id,
+    cpf,
+    name,
+    statement: [],
+  }
+
+  customers.push(newAccount);
+
+  return res.status(201).json({ status: 201, error: null, data: [] });
+});
 
 const port = 3333;
 
